14da8471484d9d63e8c0108cbb71b5a1a9c487b6
Sophie Gu <sophieqgu@gmail.com>
2021-03-09 03:43:06 +0000

    Automatically backed up by Learn

--- src/CatComponent.js
+++ src/CatComponent.js
@@ -4,6 +4,7 @@ export default class CatComponent extends Component {
   render() {
     return (
       <div className="bar" id="cat">
+        // eslint-disable-next-line
         <img src="/cat.gif" />
       </div>
     );
